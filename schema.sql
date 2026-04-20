44dad617bf6ed84ea8fa4d2383a201895353c557
Vivian Lauer <evanlauer@u.boisestate.edu>
2026-04-20 12:38:09 -0600

    Create schema

---
+++ schema.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE grades;
+USE grades;
+
+CREATE TABLE Class (
+    ID int primary key auto_increment,
+    CourseNumber varchar(50) NOT NULL,
+    Term varchar(50),
+    SectionNumber int,
+    Description text
+);
+
+CREATE TABLE Category (
+    ID int primary key auto_increment,
+    Name varchar(50) NOT NULL,
+    Weight double,
+    ClassID int,
+    foreign key (ClassID) references Class(ID) on delete cascade
+);
+
+CREATE TABLE Assignment (
+    ID int primary key auto_increment,
+    Name varchar(50) NOT NULL,
+    Description text,
+    PointValue int,
+    CategoryID int,
+    foreign key (CategoryID) references Category(ID) on delete cascade
+);
+
+CREATE TABLE Student (
+    StudentID int primary key UNIQUE NOT NULL,
+    Username varchar(50) NOT NULL,
+    Name varchar(200) NOT NULL
+);
+
+CREATE TABLE Enrolled (
+    StudentID int,
+    ClassID int,
+    primary key (StudentID, ClassID),  
+    foreign key (StudentID) references Student(StudentID) on delete cascade,
+    foreign key (ClassID) references Class(ID) on delete cascade
+);
+
+CREATE TABLE Graded (
+    StudentID int,
+    AssignmentID int,
+    primary key (StudentID, AssignmentID),
+    foreign key (StudentID) references Student(StudentID) on delete cascade,
+    foreign key (AssignmentID) references Assignment(ID) on delete cascade
+);
