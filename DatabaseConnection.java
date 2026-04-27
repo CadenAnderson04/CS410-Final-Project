996ebaa02873e79d694691c3e1bed52ac59aaae1
Vivian Lauer <evanlauer@u.boisestate.edu>
2026-04-27 02:27:24 -0600

    changed onyx.boisestate.edu to localhost

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -3,33 +3,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * This class is responsible for establishing a connection to the database. 
- * It contains a single static method, makeConnection, which returns a Connection 
- * object that can be used to interact with the database. 
- * The method handles any SQLExceptions that may occur during the connection process 
+ * This class is responsible for establishing a connection to the database.
+ * It contains a single static method, makeConnection, which returns a
+ * Connection
+ * object that can be used to interact with the database.
+ * The method handles any SQLExceptions that may occur during the connection
+ * process
  * and prints relevant error information to the console.
  */
 
 public class DatabaseConnection {
-    /**
-     * Establishes a connection to the database.
-     * @return A Connection object representing the connection to the database.
-     */
-    public static Connection makeConnection () {
-        try {
-            Connection conn = null;
-            conn = DriverManager.getConnection(
-                "jdbc:mysql://onyx.boisestate.edu:50212/grades?verifyServerCertificate=false&useSSL=true",
-                "msandbox",  
-                "NOSaints04"
-            );
-            System.out.println("Connection to database established successfully.");
-            return conn;
-        } catch (SQLException e) {
-            System.err.println("SQLException: " + e.getMessage());
-			System.err.println("SQLState: " + e.getSQLState());
-			System.err.println("VendorError: " + e.getErrorCode());
-        }
-        return null;
+  /**
+   * Establishes a connection to the database.
+   * 
+   * @return A Connection object representing the connection to the database.
+   */
+  public static Connection makeConnection() {
+    try {
+      Connection conn = null;
+      conn = DriverManager.getConnection(
+          "jdbc:mysql://localhost:50212/grades?verifyServerCertificate=false&useSSL=true",
+          "msandbox",
+          "NOSaints04");
+      System.out.println("Connection to database established successfully.");
+      return conn;
+    } catch (SQLException e) {
+      System.err.println("SQLException: " + e.getMessage());
+      System.err.println("SQLState: " + e.getSQLState());
+      System.err.println("VendorError: " + e.getErrorCode());
     }
+    return null;
+  }
 }
